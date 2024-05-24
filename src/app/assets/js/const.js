a4d7f2c6cece52e4e5a6d8e91185fc8bc55d2ef1
dongnguyentat <dong.nguyen@mycleo.net>
2024-05-24 22:25:35 +0700

    init project

---
+++ src/app/assets/js/const.js
@@ -0,0 +1,5 @@
+﻿var cartURL = "http://chivican.com/gio-hang";
+var apiURL= "http://admin.chivican.com/api/";
+var webUrlLogin = "http://localhost:7655/dang-nhap";
+var ndt_vnd = 3700;
+var serviceFee = 10000;
