f06aa91462e94266beccd1757ec1caf5ba0c1ae8
dongnguyentat <dong.nguyen@mycleo.net>
2024-08-05 22:07:38 +0700

    hotfix

--- src/app/assets/js/const.js
+++ src/app/assets/js/const.js
@@ -1,4 +1,4 @@
-﻿var cartURL = "http://chivican.com/gio-hang";
+﻿var cartURL = "https://chivican.com/gio-hang";
 var apiURL= "http://admin.chivican.com/api/";
 var webUrlLogin = "http://localhost:7655/dang-nhap";
 var ndt_vnd = 3700;
--- src/app/assets/js/service-worker.js
+++ src/app/assets/js/service-worker.js
@@ -1,6 +1,1 @@
-import { tldLocales } from './const.js';
-import { tldLocales } from './jquery/jquery.min.js';
-import { tldLocales } from './utils.js';
-import { tldLocales } from './contentScriptHelper.js';
-import { tldLocales } from './background.js';
-import { tldLocales } from './accounting.min.js';
+importScripts('./const.js', './jquery/jquery.min.js', './utils.js', './background.js', './contentScriptHelper.js', './accounting.min.js');
