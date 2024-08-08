6c8f46b9df956d36551bd644f268b13ee13932df
Dong Nguyen Tat <dong.nguyen@mycleo.net>
2024-08-08 14:03:42 +0700

    fix

--- src/app/assets/js/const.js
+++ src/app/assets/js/const.js
@@ -1,5 +1,5 @@
 ﻿var cartURL = "https://chivican.com/gio-hang";
-var apiURL= "http://admin.chivican.com/api/";
-var webUrlLogin = "http://localhost:7655/dang-nhap";
+var apiURL= "https://chivican.com/api/";
+var webUrlLogin = "https://chivican.com/dang-nhap";
 var ndt_vnd = 3700;
 var serviceFee = 10000;
